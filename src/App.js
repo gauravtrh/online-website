74bec4fc08808f3d1892814e6d2a835bb739242e
plan <plan@gmail.com>
2023-03-09 16:11:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ButtonToolbar } from 'react-bootstrap';
+import './App.css';
+import{Button}  from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <h2>online web site</h2>
+      <button>bootstrap button</button>
+    </div>
+  );
+}
+
+export default App;
